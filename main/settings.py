8410772782552b863394a0787aa1d7dda73adfdb
somtochukwu <somtochukwuuchegbu@gmail.com>
2022-04-27 20:20:58 +0100

    Edited settings.py file

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY')
+SECRET_KEY =  'django-insecure-+vh9e9lm7gvjg)l$m2s$abxa2zm&77pyz4fmyhthvey^8it&a0'
 
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
